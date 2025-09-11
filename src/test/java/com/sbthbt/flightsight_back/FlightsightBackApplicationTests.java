82da9781eb699f65143f825492704e1353a80fc5
Sebastien Hubert <hbt.sebastien@gmail.com>
2025-09-11 20:47:01 +0200

    First Commit Back

---
+++ src/test/java/com/sbthbt/flightsight_back/FlightsightBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbthbt.flightsight_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsightBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
